cc7772be79bd542b0c2c8526adf57b05d18c409f
Sivan Sasidharan <sivansasidharan@users.noreply.github.com>
2017-11-28 14:18:41 +0530

    Update RScript_02.r

--- SocialMediaAnalytics/scripts/RScript_02.r
+++ SocialMediaAnalytics/scripts/RScript_02.r
@@ -1,4 +1,4 @@
-a <- "amal"
-b <- "babu"
+a <- "test"
+b <- "me"
 z = paste(a,b,sep = ",")
-write.csv(file="C:\\Amal.Babu\\POC_01\\EclipseWorkspace\\RCaller\\output\\out_02.csv",x=z)
+write.csv(file="C:\\POC_01\\EclipseWorkspace\\RCaller\\output\\out_02.csv",x=z)
